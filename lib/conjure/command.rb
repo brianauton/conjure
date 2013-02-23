668b5912f9f6fdcfd223444b210880b05e8b6e41
Brian Auton <brianauton@gmail.com>
2013-02-22 23:36:56 -0500

    (closes #3) Spin up a VM and send it a test command

--- lib/conjure/command.rb
+++ lib/conjure/command.rb
@@ -2,7 +2,6 @@ module Conjure
   class Command < Thor
     desc "deploy", "Deploys the app"
     def deploy
-      puts "deploying..."
       Instance.new
     end
     default_task :deploy
--- lib/conjure/instance.rb
+++ lib/conjure/instance.rb
@@ -6,13 +6,28 @@ module Conjure
       config_path = File.expand_path "../../../config", __FILE__
       @vagrant = Vagrant::Environment.new :cwd => config_path
       install_base_image
-      puts "Initialized."
+      start_vm
+      issue_test_command
     end
 
     def install_base_image
       unless @vagrant.boxes.find "precise64"
         puts "Downloading 300MB Ubuntu base image for Vagrant, this may take a few minutes..."
         @vagrant.boxes.add "precise64", "http://files.vagrantup.com/precise64.box"
+      end
+    end
+
+    def start_vm
+      unless @vagrant.primary_vm.state == :running
+        puts "Starting a VM..."
+        @vagrant.primary_vm.up
+      end
+    end
+
+    def issue_test_command
+      puts "Sending the VM a command..."
+      @vagrant.primary_vm.channel.execute "uname -mrs" do |device, data|
+        puts "OS info reported by the VM: #{data}"
       end
     end
   end
