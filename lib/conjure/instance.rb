2aae7934ee638f8427cf7ee5eabf8d0dfd0f0e34
Brian Auton <brianauton@gmail.com>
2013-02-27 22:53:23 -0500

    Begin to generalize for multiple Vagrant VMs

--- lib/conjure/instance.rb
+++ lib/conjure/instance.rb
@@ -6,6 +6,7 @@ module Conjure
       config_path = File.expand_path "../../../config", __FILE__
       @vagrant = Vagrant::Environment.new :cwd => config_path
       install_base_image
+      @vm = @vagrant.primary_vm
       start_vm
       issue_test_command
     end
@@ -18,15 +19,15 @@ module Conjure
     end
 
     def start_vm
-      unless @vagrant.primary_vm.state == :running
+      unless @vm.state == :running
         puts "Starting a VM..."
-        @vagrant.primary_vm.up
+        @vm.up
       end
     end
 
     def issue_test_command
       puts "Sending the VM a command..."
-      @vagrant.primary_vm.channel.execute "uname -mrs" do |device, data|
+      @vm.channel.execute "uname -mrs" do |device, data|
         puts "OS info reported by the VM: #{data}"
       end
     end
