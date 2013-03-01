e23cd4081d2fe53c71d095bb3d9c68b349379493
Brian Auton <brianauton@gmail.com>
2013-02-28 23:45:07 -0500

    (#12) Reload vagrant to register newly installed box

--- lib/conjure/instance.rb
+++ lib/conjure/instance.rb
@@ -3,18 +3,23 @@ require "vagrant"
 module Conjure
   class Instance
     def initialize
-      config_path = File.expand_path "../../../config", __FILE__
-      @vagrant = Vagrant::Environment.new :cwd => config_path
+      @config_path = File.expand_path "../../../config", __FILE__
+      load_environment
       install_base_image
       @vm = @vagrant.primary_vm
       start_vm
       issue_test_command
     end
 
+    def load_environment
+      @vagrant = Vagrant::Environment.new :cwd => @config_path
+    end
+
     def install_base_image
       unless @vagrant.boxes.find "precise64"
         puts "Downloading 300MB Ubuntu base image for Vagrant, this may take a few minutes..."
         @vagrant.boxes.add "precise64", "http://files.vagrantup.com/precise64.box"
+        load_environment
       end
     end
 
