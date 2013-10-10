a0b836bcf6e91e72843a7c7089c9cc019ee66f19
Brian Auton <brianauton@gmail.com>
2013-10-10 11:41:21 -0400

    Simplify basic service and remove unused services

--- lib/conjure/service.rb
+++ lib/conjure/service.rb
@@ -2,33 +2,15 @@ module Conjure
   module Service
     autoload :RailsApplication, "conjure/service/rails_application"
     autoload :RailsServer, "conjure/service/rails_server"
-    autoload :RvmShell, "conjure/service/rvm_shell"
     autoload :MachineInstance, "conjure/service/machine_instance"
-    autoload :SourceTree, "conjure/service/source_tree"
     autoload :DockerHost, "conjure/service/docker_host"
     autoload :CloudServer, "conjure/service/cloud_server"
     autoload :PostgresServer, "conjure/service/postgres_server"
   end
 
   class Basic
-    def dependencies
-      []
-    end
-
-    def started?
-      false
-    end
-
-    def start
-    end
-
-    def save
-      dependencies.each &:start
-      start unless started?
-    end
-
     def self.create(*args)
-      new(*args).save
+      new(*args)
     end
   end
 end
