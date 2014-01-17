69f327ca3d63e3cdecf6e6cd40d98c65c71fb105
Brian Auton <brianauton@gmail.com>
2014-01-16 21:01:21 -0500

    Capture output streams to convert remaining command line specs

--- spec/lib/conjure/command_spec.rb
+++ spec/lib/conjure/command_spec.rb
@@ -1,6 +1,17 @@
 require "conjure"
 
 describe Conjure::Command do
+  it "shows usage info when no arguments are given" do
+    stdout = capture_stdout { invoke_with_arguments "" }
+    expect(stdout).to match("Commands:")
+    expect(stdout.lines.count).to be >= 6
+  end
+
+  it "notifies on stderr if an unknown command is given" do
+    stderr = capture_stderr { invoke_with_arguments "invalid_command" }
+    expect(stderr).to include("Could not find command")
+  end
+
   describe "'deploy' command" do
     it "allows specifying a branch to deploy with --branch" do
       invoke_with_arguments "deploy --origin /myrepo.git --branch mybranch --test"
@@ -20,5 +31,21 @@ describe Conjure::Command do
 
   def invoke_with_arguments(arguments)
     Conjure::Command.start(arguments.split " ")
+  end
+
+  def capture_stdout
+    $stdout = StringIO.new
+    yield
+    $stdout.string
+  ensure
+    $stdout = STDOUT
+  end
+
+  def capture_stderr
+    $stderr = StringIO.new
+    yield
+    $stderr.string
+  ensure
+    $stderr = STDERR
   end
 end
