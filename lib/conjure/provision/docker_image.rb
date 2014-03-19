333d40a06e1faf4d74c42a036324bda2dd9f8a2d
Brian Auton <brianauton@gmail.com>
2014-03-19 00:03:05 -0400

    Support remote server operation in Provision::DockerImage

--- lib/conjure/provision/docker_image.rb
+++ lib/conjure/provision/docker_image.rb
@@ -3,14 +3,15 @@ module Conjure
     class DockerImage
       attr_reader :image_name
 
-      def initialize(base_image_name)
+      def initialize(server, base_image_name)
+        @server = server
         @image_name = base_image_name
       end
 
       def start(shell_command, options = {})
-        container_id = `docker run -d #{options[:run_options].to_s} #{image_name} #{shell_command}`.strip
+        container_id = @server.run("docker run -d #{options[:run_options].to_s} #{image_name} #{shell_command}").strip
         sleep 2
-        ip_address = `docker inspect -format '{{ .NetworkSettings.IPAddress }}' #{container_id}`.strip
+        ip_address = @server.run("docker inspect -format '{{ .NetworkSettings.IPAddress }}' #{container_id}").strip
         raise "Container failed to start" unless ip_address.present?
         ip_address
       end
