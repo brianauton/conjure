1961180cb2626de783dbf4cb8b7ef422462248f9
Brian Auton <brianauton@gmail.com>
2014-06-01 13:43:59 -0400

    Extract Passenger service to a class

--- lib/conjure/provision/instance.rb
+++ lib/conjure/provision/instance.rb
@@ -21,8 +21,10 @@ module Conjure
         database = Postgres.new(platform)
         database.start
 
-        passenger_image = passenger_dockerfile(database).build(platform)
-        passenger_ip = passenger_image.start("/sbin/my_init", :run_options => "-p 80:80 -p 2222:22")
+        webserver = Passenger.new(platform, database, @rails_env)
+        webserver.start
+        passenger_ip = webserver.ip_address
+
         port = platform.ip_address ? "2222" : "22"
         ip_address = platform.ip_address || passenger_ip
 
@@ -36,28 +38,6 @@ module Conjure
           :user => "app",
           :rails_env => @rails_env
         }
-      end
-
-      def passenger_dockerfile(database)
-        public_key = File.expand_path("~/.ssh/id_rsa.pub")
-        raise "Error: ~/.ssh/id_rsa.pub must exist." unless File.exist?(public_key)
-        file = Docker::Template.new("conjure/passenger-ruby21:1.0.1")
-        file.add_file public_key, "/root/.ssh/authorized_keys"
-        file.add_file public_key, "/home/app/.ssh/authorized_keys"
-        file.run "chown app.app /home/app/.ssh/authorized_keys"
-        file.run "chown root.root /root/.ssh/authorized_keys"
-        file.add_file_data application_conf, "/etc/nginx/sites-enabled/application.conf"
-        file.add_file_data database_yml(database), "/home/app/application/shared/config/database.yml"
-        file
-      end
-
-      def database_yml(database)
-        {@rails_env => database.rails_config}.to_yaml
-      end
-
-      def application_conf
-        options = {listen: "80", root: "/home/app/application/current/public", passenger_enabled: "on", passenger_user: "app", passenger_ruby: "/usr/bin/ruby2.1", passenger_app_env: @rails_env}
-        "server {" + options.map{|k, v| "  #{k} #{v};"}.join("\n") + "}\n"
       end
 
       def remote_command(host, command)
---
+++ lib/conjure/provision/passenger.rb
@@ -0,0 +1,54 @@
+require "conjure/provision/docker/template"
+
+module Conjure
+  module Provision
+    class Passenger
+      attr_reader :ip_address
+
+      def initialize(platform, database, rails_env)
+        @platform = platform
+        @database = database
+        @rails_env = rails_env
+      end
+
+      def start 
+        @ip_address = dockerfile.build(@platform).start("/sbin/my_init", start_options)
+      end
+
+      private
+
+      def start_options
+        {:run_options => "-p 80:80 -p 2222:22"}
+      end
+
+      def dockerfile
+        public_key = File.expand_path("~/.ssh/id_rsa.pub")
+        raise "Error: ~/.ssh/id_rsa.pub must exist." unless File.exist?(public_key)
+        file = Docker::Template.new("conjure/passenger-ruby21:1.0.1")
+        file.add_file public_key, "/root/.ssh/authorized_keys"
+        file.add_file public_key, "/home/app/.ssh/authorized_keys"
+        file.run "chown app.app /home/app/.ssh/authorized_keys"
+        file.run "chown root.root /root/.ssh/authorized_keys"
+        file.add_file_data nginx_conf, "/etc/nginx/sites-enabled/application.conf"
+        file.add_file_data database_yml, "/home/app/application/shared/config/database.yml"
+        file
+      end
+
+      def database_yml
+        {@rails_env => @database.rails_config}.to_yaml
+      end
+
+      def nginx_conf
+        options = {
+          :listen => "80",
+          :root => "/home/app/application/current/public",
+          :passenger_enabled => "on",
+          :passenger_user => "app",
+          :passenger_ruby => "/usr/bin/ruby2.1",
+          :passenger_app_env => @rails_env,
+        }
+        "server {" + options.map{|k, v| "  #{k} #{v};"}.join("\n") + "}\n"
+      end
+    end
+  end
+end
