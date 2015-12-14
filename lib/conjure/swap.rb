ab2d2af92578004cd52425b7b9c62528c66aaa3c
Brian Auton <brianauton@gmail.com>
2015-12-14 13:11:14 -0500

    Check for presence of swap before installing

--- lib/conjure/swap.rb
+++ lib/conjure/swap.rb
@@ -5,9 +5,20 @@ module Conjure
     end
 
     def install
-      puts "Installing swap space..."
-      @server.run "dd if=/dev/zero of=/root/swapfile bs=4096 count=524288"
-      @server.run "mkswap /root/swapfile; swapon /root/swapfile"
+      if exists?
+        puts "Swap space detected."
+      else
+        puts "Swap space not detected, installing..."
+        @server.run "dd if=/dev/zero of=/root/swapfile bs=4096 count=524288"
+        @server.run "mkswap /root/swapfile; swapon /root/swapfile"
+        puts "Swap space installed."
+      end
+    end
+
+    private
+
+    def exists?
+      @server.run("swapon -s | wc -l").to_i > 1
     end
   end
 end
